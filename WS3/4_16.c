09eec5ef53f29a9c72dc5282ae1f39b9d6f429d0
PrsnVkngs <oskaraattar@gmail.com>
2023-02-15 12:33:13 -0800

    Completed 4.16 for Assignment 3

--- WS3/4_16.c
+++ WS3/4_16.c
@@ -2,4 +2,24 @@
 
 int main() {
 
-	int[] 
+	int sum = 0, max = 0, cnt = -1, num = 0;
+
+	while ( num >= 0 ) {
+
+		cnt++;
+
+		if ( num > max ) {
+			max = num;
+		}
+
+		sum += num;
+
+		scanf("%d", &num);
+
+	}
+
+	printf("%d %0.2lf\n", max, (double)sum/cnt);
+
+	return 0;
+
+}
